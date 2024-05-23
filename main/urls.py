1c9554d576731d6dd1ad45501a5c655e0a9274e0
hesham <gwtw.mhn@gmail.com>
2024-05-23 15:47:01 +0500

    User Profile section and minor UI changes

--- main/urls.py
+++ main/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('create_post/', views.create_post, name='createpost'),
     path('user_dashboard/', views.show_dashboard, name='userdashboard'),
+    path('user_profile/<str:username>/', views.user_profile, name='user_profile'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import Registaraionform, PostForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required, permission_required
@@ -12,6 +12,7 @@ def home(request):
     if request.method == "POST":
         post_id = request.POST.get('post_id')
         user_id = request.POST.get('user_id')
+        profile_id = request.POST.get('profile_id')
 
 
         if post_id:
@@ -32,11 +33,27 @@ def home(request):
                     group.user_set.remove(user)
                 except:
                     pass
-
-
+        elif profile_id:
+            return redirect('user_profile', username=profile_id)
     posts = Post.objects.all()
 
     return render(request, 'main/homepage.html', {'posts':posts})
+
+
+@login_required(login_url='/login')
+def user_profile(request, username):
+    # Retrieve the user object based on the username passed in the URL
+    user = get_object_or_404(User, username=username)
+    
+    # Fetch all the posts associated with that user
+    posts = Post.objects.filter(author=user)
+    
+    # Render the user_profile.html template with the posts
+    return render(request, 'main/user_profile.html', {'user': user, 'posts': posts})
+
+
+
+
 
 
 @permission_required('main.add_post', login_url='/login', raise_exception=True)
@@ -60,6 +77,11 @@ def show_dashboard(request):
     # Filter posts by the currently logged-in user
     posts = Post.objects.filter(author=request.user)
     return render(request, 'main/dashboard.html', {'posts': posts})
+
+
+
+
+
 
 def signup(request):
     if request.method == 'POST':
