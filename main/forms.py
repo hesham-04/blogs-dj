1aa38bdd5aa0bf193356ecccb8544d3ef11339b3
hesham <gwtw.mhn@gmail.com>
2024-05-23 14:14:05 +0500

    Version 1

---
+++ main/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post
+
+
+
+class Registaraionform(UserCreationForm):
+    email = forms.EmailField(max_length=255, required=True)
+
+    class Meta:
+        model= User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+            model = Post
+            fields = ['title', 'description']
+
---
+++ main/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from .forms import Registaraionform, PostForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Post
+from django.contrib.auth.models import User, Group
+
+
+# Create your views here.
+@login_required(login_url='/login')
+def home(request):
+    if request.method == "POST":
+        post_id = request.POST.get('post_id')
+        user_id = request.POST.get('user_id')
+
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm('main.delete_post')):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(pk=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+
+    posts = Post.objects.all()
+
+    return render(request, 'main/homepage.html', {'posts':posts})
+
+
+@permission_required('main.add_post', login_url='/login', raise_exception=True)
+@login_required(login_url='/login')
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+    else:
+        form = PostForm(request.POST)
+        return render(request, 'main/create_post.html', {'form':form})
+
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = Registaraionform(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = Registaraionform()
+    return render(request, 'main/signup.html', {'form': form})
